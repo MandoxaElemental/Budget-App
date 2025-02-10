0ed0f4b7fe78326067ba0831b9068d3fdc967e69
Gianpaolo Reinares <gprcards@gmail.com>
2025-02-10 12:05:28 -0800

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,51 @@
+import { getLocalStorage, saveToLocalStorageByExpenses, removeFromLocalStorage, saveBudget, getBudget } from "./localStorage.js";
+
+let MyBudget = 0;
+let UpdateBtn = document.getElementById('updateBtn');
+let ManageBtn = document.getElementById('manageBtn');
+let AddBtn = document.getElementById('addBtn');
+let BudgetInput = document.getElementById('budgetInput');
+let UpdateChangeBtn = document.getElementById('updateChangeBtn');
+let UpdateCancelBtn = document.getElementById('updateCancelBtn');
+let UpdateBox = document.getElementById('UpdateBox');
+let ManageChangeBtn = document.getElementById('manageChangeBtn');
+let ManageCancelBtn = document.getElementById('manageCancelBtn');
+let ManageBox = document.getElementById('ManageBox');
+let AddChangeBtn = document.getElementById('addChangeBtn');
+let AddCancelBtn = document.getElementById('addCancelBtn');
+let AddBox = document.getElementById('AddBox');
+
+UpdateBtn.addEventListener("click", async () => {
+    UpdateBox.className = "mainBox absolute fadeIn"
+})
+UpdateCancelBtn.addEventListener("click", async () => {
+    UpdateBox.className = "hidden"
+})
+ManageBtn.addEventListener("click", async () => {
+    ManageBox.className = "mainBox absolute fadeIn"
+})
+ManageCancelBtn.addEventListener("click", async () => {
+    ManageBox.className = "hidden"
+})
+AddBtn.addEventListener("click", async () => {
+    AddBox.className = "mainBox absolute fadeIn"
+})
+AddCancelBtn.addEventListener("click", async () => {
+    AddBox.className = "hidden"
+})
+
+UpdateChangeBtn.addEventListener("click", async () => {
+    let ConvertedNum = Number(BudgetInput.value);
+    if (ConvertedNum >= 0) {
+        MyBudget = ConvertedNum;
+        console.log('Correct')
+        console.log(MyBudget)
+        document.getElementById('theBudget').innerText = `${MyBudget}`
+        document.getElementById('currentBudget').innerText = `${MyBudget}`
+        saveBudget(ConvertedNum); 
+        
+    } else {
+        console.log('error')
+    }
+});
+
---
+++ scripts/localStorage.js
@@ -0,0 +1,50 @@
+function saveBudget(budget) {
+    let budgetArr = getBudget();
+
+    if (!budgetArr.includes(budget)) {
+        budgetArr.push(budget);
+    }
+    localStorage.setItem('Budget', JSON.stringify(budgetArr));
+}
+
+function getBudget(){
+
+    let localBudgetData = localStorage.getItem('Budget');
+
+    if (localBudgetData == null) {
+        return [];
+    }
+    return JSON.parse(localBudgetData);
+}
+
+function saveToLocalStorageByExpenses(expenses) {
+    let expensesArr = getLocalStorage();
+
+    if (!expensesArr.includes(expenses)) {
+        expensesArr.push(expenses);
+    }
+    
+
+    localStorage.setItem('Expenses', JSON.stringify(expensesArr));
+}
+
+function getLocalStorage(){
+
+    let localStorageData = localStorage.getItem('Expenses');
+
+    if (localStorageData == null) {
+        return [];
+    }
+    return JSON.parse(localStorageData);
+}
+
+function removeFromLocalStorage(expenses){
+    let expensesArr = getLocalStorage();
+
+
+    let expensesindex = expensesArr.indexOf(expenses);
+    expensesArr.splice(expensesindex, 1);
+    localStorage.setItem('Expenses', JSON.stringify(expensesArr));
+}
+
+export{saveToLocalStorageByExpenses, getLocalStorage, removeFromLocalStorage, saveBudget, getBudget }
